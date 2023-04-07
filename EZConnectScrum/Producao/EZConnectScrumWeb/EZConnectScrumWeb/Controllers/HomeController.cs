3ad01c8c217e0c000b4420951345bcb7f26d96ef
Marcio Martins <marciopmm@gmail.com>
2023-04-06 22:01:20 -0300

    Adding EZConnect

---
+++ EZConnectScrum/Producao/EZConnectScrumWeb/EZConnectScrumWeb/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EZConnectScrumWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Este é um exemplo de utilização do TFS Scrum Template.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Exemplo Arquitetura C#";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Visite nosso site!";
+
+            return View();
+        }
+    }
+}
